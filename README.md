a3f4da4ca9887e8870bba7b2a93391208291697a
kodevthkrushi <krushivalnalike27@gmail.com>
2025-11-01 11:10:36 +0530

    initial files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vihari Project
+“Every journey begins with Vihari.”
+
+This is a travel listings web app built using **Node.js**, **Express**, **MongoDB**, and **EJS**.  
+It allows users to create, view, and manage travel listings.
+
+## Features
+- Create new travel listings  
+- View listing details  
+- Edit or delete listings  
+- Responsive layout with Bootstrap
+
+## Tech Stack
+- Node.js  
+- Express  
+- MongoDB (Mongoose)  
+- EJS Templates  
+- Bootstrap
+
+## Author
+Krushival
---
+++ models/listings.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1747447597297-0716bbd5b049?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764",
+        set: (v) => 
+            v === "" 
+            ? "https://images.unsplash.com/photo-1747447597297-0716bbd5b049?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764" 
+            : v,       
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
